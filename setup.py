2407ca37ba51833a12b93b90cefcd740dbc8154a
WolfgangSpahn <wolfgang.spahn@gmail.com>
2024-03-26 10:34:55 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='depcy',
     version='0.1.0',
-    packages=['depcy'],
+    packages=find_packages(),
     install_requires=[
         'spacy',
         'transformers',
